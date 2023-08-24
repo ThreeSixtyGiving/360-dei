65aaf39c74abf0625bc8d77ff87b5a589f12cec5
Matt Marshall <matt@mrshll.uk>
2023-08-24 10:27:31 +0100

    fix typo

--- documentation/conf.py
+++ documentation/conf.py
@@ -39,7 +39,7 @@ html_context = {
     'theme_navigation_depth': 2
 }
 
-html_theme = 'spinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for

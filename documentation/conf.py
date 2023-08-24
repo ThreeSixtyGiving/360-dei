f9894a35dc499c77dd004fd2a0ca2870464e20e8
Matt Marshall <matt@mrshll.uk>
2023-08-24 10:24:57 +0100

    conf.py edit

--- documentation/conf.py
+++ documentation/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 import datetime
 import flattentool
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -37,6 +38,10 @@ html_context = {
     'now': datetime.datetime.now(),
     'theme_navigation_depth': 2
 }
+
+html_theme = 'spinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
